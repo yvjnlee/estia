05c9583012245225f36a5b6c8764cbce05daefa0
Abeer Das <abeerdas647@gmail.com>
2024-09-16 11:09:33 -0400

    changed login css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,10 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { createClient, Session } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 const supabase = createClient(
   process.env.REACT_APP_PROJECT_URL as string,
-  process.env.REACT_APP_ANON_KEY as string,
+  process.env.REACT_APP_ANON_KEY as string
 );
 
 export default function App() {
@@ -28,12 +27,50 @@ export default function App() {
   }, []);
 
   if (!session) {
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+    return (
+      <div className="login-container">
+        <h2 className="login-title">Create an account</h2>
+      <Auth
+        supabaseClient={supabase}
+        appearance={{
+          extend: false,
+          className: {
+            container: "custom-container",
+            button: "custom-button",
+            input: "custom-input",
+            anchor: "custom-anchor",
+            label: "custom-label",
+            message:"custom_message",
+          },
+          style: {
+            // Add any inline styles if needed, otherwise, the CSS classes will handle the styles.
+          },
+        }}
+        localization={{
+          variables: {
+            sign_in: {
+              email_label: '',
+              password_label: '',
+              email_input_placeholder: "Email",
+              password_input_placeholder: "Password",
+            },
+            sign_up: {
+              email_label: '',
+              password_label: '',
+              password_input_placeholder: "Password",
+            },
+          },
+        }}
+      />
+            </div>
+
+    );
   } else {
     return (
       <>
         <div>Logged in!</div>
         <button
+          className="custom-button"
           onClick={async () => {
             await supabase.auth.signOut();
           }}
