afde05278cf512a440854884b41f43e7b7f6ade7
Yu Sun <anne.sun@utoronto.ca>
2024-10-02 14:43:17 -0400

    Styling for comment/discussions section

--- client/src/components/project/Comments.tsx
+++ client/src/components/project/Comments.tsx
@@ -1,22 +1,17 @@
 import {useState} from 'react';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ReplyIcon from '@mui/icons-material/Reply';
+
 const Comments: React.FC = () => {
-    // const [focused, setFocused] = useState<boolean>(false);
-    // const handleFocus = () => {
-    //     setFocused(true);
+    // const [voteUpHovered, setUpHovered] = useState<boolean>(false);
+    // const [voteDownHovered, setDownHovered] = useState<boolean>(false);
+    // const voteUpHover = () => {
+    //     setUpHovered(!voteUpHovered);
     // }
-    // const handleBlur = () => {
-    //     setFocused(false);
+    // const voteDownHover = () => {
+    //     setDownHovered(!voteDownHovered);
     // }
-    const [voteUpHovered, setUpHovered] = useState<boolean>(false);
-    const [voteDownHovered, setDownHovered] = useState<boolean>(false);
-    const voteUpHover = () => {
-        setUpHovered(!voteUpHovered);
-    }
-    const voteDownHover = () => {
-        setDownHovered(!voteDownHovered);
-    }
     
     const [votes, setVotes] = useState(0); // fetch votes from backend
     const voteUp = () => {
@@ -34,30 +29,33 @@ const Comments: React.FC = () => {
         setComment(event.target.value);
     }
     return (
-        <div className="comment-container">
+        <div className='comment-container'>
             
             <h1> Discussion (20)</h1>
                 <div className="discussion-input">
                     <input type='text' placeholder={comment} onChange={updateComment} required></input>
-                    <label htmlFor=''>Comment</label>
-                    <button className="submit-comment-button" onClick={postComment}>Post</button>
+                    <label htmlFor=''>Write text here...</label>
+                    <button onClick={postComment}>Post</button>
                 </div>
 
             
             <div className="comment-section">
+                <div className="vote">
+                          <button  onClick={voteUp}><ExpandLessIcon fontSize='medium'/></button>
+                        <span>{votes}</span>
+                        <button  onClick={voteDown}><ExpandMoreIcon fontSize='medium' /></button>
+                </div>
                 <div className="comment">
                     <h3>michaelzhou1232</h3>
-                    <div className="vote">
-                        <button onMouseEnter={voteUpHover} onMouseLeave={voteUpHover} onClick={voteUp}><ExpandLessIcon fontSize={voteUpHovered ? 'medium' : 'small'} /></button>
-                        <span>{votes}</span>
-                        <button onMouseEnter={voteDownHover} onMouseLeave={voteDownHover} onClick={voteDown}><ExpandMoreIcon fontSize={voteDownHovered ? 'medium' : 'small'} /></button>
-                    </div>
+                    <p>
+                    For the people who have the account page black when you just create the account...
+                    </p>
+                    <button className="reply-btn">
+                        <ReplyIcon style = {{paddingRight: '0.5rem'}}/>
+                        Reply
+                    </button>
+                </div>
                 
-                <p>
-                For the people who have the account page black when you just create the account...
-                </p>
-                <button className="reply-btn">Reply</button>
-            </div>
             </div>
         </div>
     );
